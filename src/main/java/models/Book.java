114429f103063a882d82685955e6c54f74ea91c5
Gregor Cox <gregorcox@gmail.com>
2018-07-18 12:02:28 +0100

    creates book class

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,71 @@
+package models;
+
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean onLoan;
+    private Borrower borrower;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(){}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column (name="author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Column(name = "On Loan?")
+    public boolean isOnLoan() {
+        return onLoan;
+    }
+
+    public void setOnLoan(boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="borrower_id", nullable = false)
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+}
